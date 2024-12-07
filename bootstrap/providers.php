bf1fca8d43677e90e7103227165ad907928e0449
Josh Cirre <joshcirre@gmail.com>
2024-12-06 23:09:47 -0700

    Initial commit

---
+++ bootstrap/providers.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    App\Providers\AppServiceProvider::class,
+    App\Providers\FolioServiceProvider::class,
+    App\Providers\SoloServiceProvider::class,
+    App\Providers\VoltServiceProvider::class,
+];
